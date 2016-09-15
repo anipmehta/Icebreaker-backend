5c952f8bd92d9ca67b2531ae26c1c8cc0949af8d
Anip Mehta <anipmehta13@gmail.com>
2016-09-16 04:21:56 +0530

    changed database

--- icebreaker_backend/settings.py
+++ icebreaker_backend/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'icebreaker_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'icebreaker_backend',
+        'USER': 'admin',
+        'PASSWORD': 'icebreaker',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
